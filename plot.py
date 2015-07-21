c63788fb9b359053cf8b659715df270635db3c1d
RyogaLi <ryogali@hotmail.com>
2015-07-21 14:45:25 -0700

    New plot.py

--- plot.py
+++ plot.py
@@ -1,21 +1,27 @@
 import matplotlib.pyplot as plt
+def plot(filename):
+        """
+        Take out the mapping position (start position) from a formated text file and save them in (x,y)
+        """
+        x = []
+        y = []
+        with open(filename, "r") as inputfile:
+                for line in inputfile:
+                        line = line.split()
+                        x.append(int(line[7]))
+                        y.append(int(line[3]))
+                        inputfile.next()
+        # inputfile.close()
+        filename2 = filename.split('.')
+        plt.clf()
+        plt.xlabel(filename2[1])
+        plt.ylabel(filename2[0])
+        plt.title('Eliminated Low Complexity Region')
+        plt.plot(x, y, 'ro')
+        figure = plt.gcf()
+        figure.savefig(filename2[0]+'.'+filename2[1] + '.png', dpi=200)
+        return
+        
 
-def coordinates(filename):
-	"""
-	Take out the mapping position (start position) from a formated text file and save them in (x,y)
-	"""
-	x = []
-	y = []
-	with open(filename, "r") as inputfile:
-		for line in inputfile:
-			line = line.split()
-			x.append(line[3])
-			y.append(line[7])
-			inputfile.next()
-		return (x, y)
-
-def generateGraph(tuple):
-	"""
-	"""
-
-
+plot("chrV.chrXII.matepair")
+plot("chrV.chrXIV.matepairs")
